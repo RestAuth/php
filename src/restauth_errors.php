fd644e07a36d139018e0cea4ba6a1e1219006ff0
Mathias Ertl <mati@fsinf.at>
2010-10-06 13:57:30 +0000

    add @package tags

--- src/restauth_errors.php
+++ src/restauth_errors.php
@@ -1,19 +1,32 @@
 <?php
 
 /**
+ * This file collects all exceptions not directly related to a RestAuthUser or
+ * a RestAuthGroup
+ *
+ * @package php-restauth
+ */
+
+/**
  * Common superclass for all RestAuth related exceptions.
+ *
+ * @package php-restauth
  */
 class RestAuthException extends Exception {
 }
 
 /**
  * Superclass for exceptions thrown when a resource queried is not found.
+ *
+ * @package php-restauth
  */
 class RestAuthResourceNotFound extends RestAuthException {
 }
 
 /**
  * Superclass for service-related errors.
+ *
+ * @package php-restauth
  */
 class RestAuthInternalException extends RestAuthException {
 }
@@ -21,6 +34,8 @@ class RestAuthInternalException extends RestAuthException {
 /**
  * Thrown when the RestAuth service cannot parse the HTTP request. On a protocol
  * level, this corresponds to a HTTP status code 400.
+ *
+ * @package php-restauth
  */
 class RestAuthBadRequest extends RestAuthInternalException {
 }
@@ -28,6 +43,8 @@ class RestAuthBadRequest extends RestAuthInternalException {
 /**
  * Thrown when the RestAuth service suffers an internal error. On a protocol
  * level, this corresponds to a HTTP status code 500.
+ *
+ * @package php-restauth
  */
 class RestAuthInternalServerError extends RestAuthInternalException {
 }
@@ -35,6 +52,8 @@ class RestAuthInternalServerError extends RestAuthInternalException {
 /**
  * Thrown when an unknown HTTP status code is encountered. This should never
  * really happen and usually indicates a bug in the library.
+ *
+ * @package php-restauth
  */
 class RestAuthUnknownStatus extends RestAuthInternalException {
 }
@@ -42,6 +61,8 @@ class RestAuthUnknownStatus extends RestAuthInternalException {
 /**
  * Superclass of exceptions thrown when a resource is supposed to be created but
  * already exists.
+ *
+ * @package php-restauth
  */
 class RestAuthResourceConflict extends RestAuthException { 
 }
@@ -49,12 +70,16 @@ class RestAuthResourceConflict extends RestAuthException {
 /**
  * Thrown when you send unacceptable data to the RestAuth service, i.e. a
  * password that is too short.
+ *
+ * @package php-restauth
  */
 class RestAuthDataUnacceptable extends RestAuthException {
 }
 
 /**
  * Superclass for exceptions related to access for this service.
+ *
+ * @package php-restauth
  */
 class RestAuthServiceAuthorizationException extends RestAuthException {
 }
@@ -63,6 +88,8 @@ class RestAuthServiceAuthorizationException extends RestAuthException {
  * Thrown when the user/password does not match the registered service.
  *
  * On a protocol level, this corresponds to the HTTP status code 401.
+ *
+ * @package php-restauth
  */
 class RestAuthUnauthorized extends RestAuthServiceAuthorizationException {
 }
@@ -71,6 +98,8 @@ class RestAuthUnauthorized extends RestAuthServiceAuthorizationException {
  * Thrown when service authentication failed and is not possible from this host.
  *
  * On a protocol level, this corresponds to the HTTP status code 403.
+ *
+ * @package php-restauth
  */
 class RestAuthForbidden extends RestAuthServiceAuthorizationException {
 }
