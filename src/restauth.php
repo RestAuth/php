581a4c2c0ae6abdcb3c529991ca31327ec04ea43
Mathias Ertl <mati@fsinf.at>
2010-10-06 13:56:53 +0000

    minor documentation

--- src/restauth.php
+++ src/restauth.php
@@ -1,4 +1,13 @@
 <?php
+/**
+ * This file servers as a convenience file which imports the entire library.
+ *
+ * @package php-restauth
+ */
+
+/**
+ * imports everything.
+ */
 require_once( 'restauth_common.php' );
 require_once( 'restauth_errors.php' );
 require_once( 'restauth_users.php' );
