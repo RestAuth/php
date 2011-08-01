5a63962cb97293cea5d1470606ff0e37b6e642a1
Mathias Ertl <mati@fsinf.at>
2011-08-01 21:20:42 +0200

    fix all PEAR coding style violations that do not require refactoring

--- RestAuth/restauth_groups.php
+++ RestAuth/restauth_groups.php
@@ -2,19 +2,46 @@
 /**
  * Code related to RestAuthGroup handling.
  *
- * @package php-restauth
+ * PHP version 5.1
+ *
+ * LICENSE: php-restauth is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * php-restauth is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with php-restauth.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Authentication
+ * @package   RestAuth
+ * @author    Mathias Ertl <mati@restauth.net>
+ * @copyright 2010-2011 Mathias Ertl
+ * @license   http://www.gnu.org/licenses/lgpl.html  GNU LESSER GENERAL PUBLIC LICENSE
+ * @version   0.0
+ * @link      https://php.restauth.net
  */
 
 /**
  * imports required for the code here.
  */
-require_once('restauth_common.php');
-require_once('restauth_users.php');
+require_once 'restauth_common.php';
+require_once 'restauth_users.php';
 
 /**
  * Thrown when a group that is supposed to be created already exists.
  *
- * @package php-restauth
+ * @category  Authentication
+ * @package   RestAuth
+ * @author    Mathias Ertl <mati@restauth.net>
+ * @copyright 2010-2011 Mathias Ertl
+ * @license   http://www.gnu.org/licenses/lgpl.html  GNU LESSER GENERAL PUBLIC LICENSE
+ * @version   Release: @package_version@
+ * @link      https://php.restauth.net
  */
 class RestAuthGroupExists extends RestAuthResourceConflict
 {
@@ -23,7 +50,13 @@ class RestAuthGroupExists extends RestAuthResourceConflict
 /**
  * This class acts as a frontend for actions related to groups.
  *
- * @package php-restauth
+ * @category  Authentication
+ * @package   RestAuth
+ * @author    Mathias Ertl <mati@restauth.net>
+ * @copyright 2010-2011 Mathias Ertl
+ * @license   http://www.gnu.org/licenses/lgpl.html  GNU LESSER GENERAL PUBLIC LICENSE
+ * @version   Release: @package_version@
+ * @link      https://php.restauth.net
  */
 class RestAuthGroup extends RestAuthResource
 {
@@ -33,9 +66,9 @@ class RestAuthGroup extends RestAuthResource
      * Factory method that creates a new group in RestAuth.
      * 
      * @param RestAuthConnection $conn A connection to a RestAuth service.
-     * @param string $name The name of the new group.
+     * @param string             $name The name of the new group.
      *
-     * @param string $name The name of the new group
+     * @return RestAuthGroup An instance representing a remote group.
      *
      * @throws {@link RestAuthBadRequest} When the request body could not be
      *    parsed.
@@ -52,18 +85,18 @@ class RestAuthGroup extends RestAuthResource
     {
         $resp = $conn->post('/groups/', array('group' => $name));
         switch ($resp->getResponseCode()) {
-            case 201:
-                return new RestAuthGroup($conn, $name);
-                
-            case 409:
-                throw new RestAuthGroupExists($resp);
-                
-            case 412:
-                throw new RestAuthPreconditionFailed($resp);
-                
+        case 201:
+            return new RestAuthGroup($conn, $name);
+            
+        case 409:
+            throw new RestAuthGroupExists($resp);
+            
+        case 412:
+            throw new RestAuthPreconditionFailed($resp);
+            
             // @codeCoverageIgnoreStart
-            default:
-                throw new RestAuthUnknownStatus($resp);
+        default:
+            throw new RestAuthUnknownStatus($resp);
         }
         // @codeCoverageIgnoreEnd
     }
@@ -73,7 +106,9 @@ class RestAuthGroup extends RestAuthResource
      * exists.
      * 
      * @param RestAuthConnection $conn A connection to a RestAuth service.
-     * @param string $name The name of the new group.
+     * @param string             $name The name of the new group.
+     *
+     * @return RestAuthGroup An instance representing a remote group.
      *
      * @throws {@link RestAuthUnauthorized} When service authentication failed.
      * @throws {@link RestAuthResourceNotFound} When the group does not exist.
@@ -85,15 +120,15 @@ class RestAuthGroup extends RestAuthResource
     {
         $resp = $conn->get('/groups/' . $name . '/');
         switch ($resp->getResponseCode()) {
-            case 204:
-                return new RestAuthGroup($conn, $name);
-                
-            case 404:
-                throw new RestAuthResourceNotFound($resp);
-                
+        case 204:
+            return new RestAuthGroup($conn, $name);
+            
+        case 404:
+            throw new RestAuthResourceNotFound($resp);
+            
             // @codeCoverageIgnoreStart
-            default:
-                throw new RestAuthUnknownStatus($resp);
+        default:
+            throw new RestAuthUnknownStatus($resp);
         }
         // @codeCoverageIgnoreEnd
     }
@@ -102,9 +137,9 @@ class RestAuthGroup extends RestAuthResource
      * Factory method that gets all groups for this service known to RestAuth.
      *
      * @param RestAuthConnection $conn A connection to a RestAuth service.
-     * @param mixed $user Limit the output to groups where the given user is a
-     *    member of. Either a {@link RestAuthUser} or a string representing the
-     *    name of that user.
+     * @param mixed              $user Limit the output to groups where the
+     *    given user is a member of. Either a {@link RestAuthUser} or a string
+     *    representing the name of that user.
      *
      * @return array Array of {@link RestAuthGroup groups}.
      *
@@ -116,7 +151,7 @@ class RestAuthGroup extends RestAuthResource
      *    returns HTTP status code 500
      * @throws {@link RestAuthUnknownStatus} If the response status is unknown.
      */
-    public static function get_all($conn, $user=NULL)
+    public static function get_all($conn, $user=null)
     {
         $params = array();
         if ($user) {
@@ -129,19 +164,19 @@ class RestAuthGroup extends RestAuthResource
     
         $resp = $conn->get('/groups/', $params);
         switch ($resp->getResponseCode()) {
-            case 200: 
-                $groups = array();
-                foreach (json_decode($resp->getBody()) as $groupname) {
-                    $groups[] = new RestAuthGroup($conn, $groupname);
-                }
-                return $groups;
+        case 200: 
+            $groups = array();
+            foreach (json_decode($resp->getBody()) as $groupname) {
+                $groups[] = new RestAuthGroup($conn, $groupname);
+            }
+            return $groups;
+        
+        case 404:
+            throw new RestAuthResourceNotFound($resp);
             
-            case 404:
-                throw new RestAuthResourceNotFound($resp);
-                
             // @codeCoverageIgnoreStart
-            default:
-                throw new RestAuthUnknownStatus($resp);
+        default:
+            throw new RestAuthUnknownStatus($resp);
         }
         // @codeCoverageIgnoreEnd
     }
@@ -151,7 +186,7 @@ class RestAuthGroup extends RestAuthResource
      * The constructor does not make sure the group exists. 
      * 
      * @param RestAuthConnection $conn A connection to a RestAuth service.
-     * @param string $name The name of the new group.
+     * @param string             $name The name of the new group.
      */
     public function __construct($conn, $name)
     {
@@ -178,19 +213,19 @@ class RestAuthGroup extends RestAuthResource
 
         $resp = $this->_get($this->name . '/users/', $params);
         switch ($resp->getResponseCode()) {
-            case 200: 
-                $users = array();
-                foreach (json_decode($resp->getBody()) as $username) {
-                    $users[] = new RestAuthUser($this->conn, $username);
-                }
-                return $users;
+        case 200: 
+            $users = array();
+            foreach (json_decode($resp->getBody()) as $username) {
+                $users[] = new RestAuthUser($this->conn, $username);
+            }
+            return $users;
+        
+        case 404:
+            throw new RestAuthResourceNotFound($resp);
             
-            case 404:
-                throw new RestAuthResourceNotFound($resp);
-                
             // @codeCoverageIgnoreStart
-            default:
-                throw new RestAuthUnknownStatus($resp);
+        default:
+            throw new RestAuthUnknownStatus($resp);
         }
         // @codeCoverageIgnoreEnd
     }
@@ -200,6 +235,8 @@ class RestAuthGroup extends RestAuthResource
      *
      * @param mixed $user The user to add. Either a {@link RestAuthUser} or a
      *    string.
+     *
+     * @return null
      *
      * @throws {@link RestAuthBadRequest} When the request body could not be
      *    parsed.
@@ -221,15 +258,15 @@ class RestAuthGroup extends RestAuthResource
 
         $resp = $this->_post($this->name . '/users/', $params);
         switch ($resp->getResponseCode()) {
-            case 204:
-                return;
+        case 204:
+            return;
+        
+        case 404:
+            throw new RestAuthResourceNotFound($resp);
             
-            case 404:
-                throw new RestAuthResourceNotFound($resp);
-                
             // @codeCoverageIgnoreStart
-            default:
-                throw new RestAuthUnknownStatus($resp);
+        default:
+            throw new RestAuthUnknownStatus($resp);
         }
         // @codeCoverageIgnoreEnd
     }
@@ -239,6 +276,7 @@ class RestAuthGroup extends RestAuthResource
      *
      * @param mixed $user The user to test. Either a  {@link RestAuthUser} or a
      *    string representing the username.
+     *    
      * @return boolean true if the user is a member, false if not
      *
      * @throws {@link RestAuthUnauthorized} When service authentication failed.
@@ -259,27 +297,29 @@ class RestAuthGroup extends RestAuthResource
         $resp = $this->_get($url);
 
         switch ($resp->getResponseCode()) {
-            case 204:
-                return true;
+        case 204:
+            return true;
+        
+        case 404:
+            switch ($resp->getHeader('Resource-Type')) {
+            case 'user':
+                return false;
             
-            case 404:
-                switch ($resp->getHeader('Resource-Type')) {
-                    case 'user':
-                        return false;
-                    
-                    default: 
-                        throw new RestAuthResourceNotFound($resp);
-                }
-                
-            // @codeCoverageIgnoreStart
             default:
-                throw new RestAuthUnknownStatus($resp);
+                throw new RestAuthResourceNotFound($resp);
+            }
+            
+            // @codeCoverageIgnoreStart
+        default:
+            throw new RestAuthUnknownStatus($resp);
         }
         // @codeCoverageIgnoreEnd
     }
 
     /**
      * Delete this group.
+     *
+     * @return null
      *
      * @throws {@link RestAuthUnauthorized} When service authentication failed.
      * @throws {@link RestAuthResourceNotFound} When the group does not exist.
@@ -291,15 +331,15 @@ class RestAuthGroup extends RestAuthResource
     {
         $resp = $this->_delete($this->name);
         switch ($resp->getResponseCode()) {
-            case 204:
-                return;
+        case 204:
+            return;
+        
+        case 404:
+            throw new RestAuthResourceNotFound($resp);
             
-            case 404:
-                throw new RestAuthResourceNotFound($resp);
-                
             // @codeCoverageIgnoreStart
-            default:
-                throw new RestAuthUnknownStatus($resp);
+        default:
+            throw new RestAuthUnknownStatus($resp);
         }
         // @codeCoverageIgnoreEnd
     }
@@ -309,6 +349,8 @@ class RestAuthGroup extends RestAuthResource
      *
      * @param mixed $user The user to remove. Either a {@link RestAuthUser} or a
      * string the username.
+     *
+     * @return null
      *
      * @throws {@link RestAuthUnauthorized} When service authentication failed.
      * @throws {@link RestAuthResourceNotFound} When the group or user does not
@@ -329,15 +371,15 @@ class RestAuthGroup extends RestAuthResource
         $resp = $this->_delete($url);
 
         switch ($resp->getResponseCode()) {
-            case 204:
-                return;
+        case 204:
+            return;
+        
+        case 404:
+            throw new RestAuthResourceNotFound($resp);
             
-            case 404:
-                throw new RestAuthResourceNotFound($resp);
-                
             // @codeCoverageIgnoreStart
-            default:
-                throw new RestAuthUnknownStatus($resp);
+        default:
+            throw new RestAuthUnknownStatus($resp);
         }
         // @codeCoverageIgnoreEnd
     }
@@ -347,6 +389,8 @@ class RestAuthGroup extends RestAuthResource
      *
      * @param mixed $group The group to add. Either a {@link RestAuthGroup} or a
      *    string representing the groupname.
+     *
+     * @return null
      *
      * @throws {@link RestAuthBadRequest} When the request body could not be
      *    parsed.
@@ -371,15 +415,15 @@ class RestAuthGroup extends RestAuthResource
         
         $resp = $this->_post($this->name . '/groups/', $params);
         switch ($resp->getResponseCode()) {
-            case 204:
-                return;
+        case 204:
+            return;
+        
+        case 404:
+            throw new RestAuthResourceNotFound($resp);
             
-            case 404:
-                throw new RestAuthResourceNotFound($resp);
-                
             // @codeCoverageIgnoreStart
-            default:
-                throw new RestAuthUnknownStatus($resp);
+        default:
+            throw new RestAuthUnknownStatus($resp);
         }
         // @codeCoverageIgnoreEnd
     }
@@ -401,19 +445,19 @@ class RestAuthGroup extends RestAuthResource
     {
         $resp = $this->_get($this->name . '/groups/');
         switch ($resp->getResponseCode()) {
-            case 200: 
-                $users = array();
-                foreach (json_decode($resp->getBody()) as $username) {
-                    $users[] = new RestAuthGroup($this->conn, $username);
-                }
-                return $users;
+        case 200: 
+            $users = array();
+            foreach (json_decode($resp->getBody()) as $username) {
+                $users[] = new RestAuthGroup($this->conn, $username);
+            }
+            return $users;
+        
+        case 404:
+            throw new RestAuthResourceNotFound($resp);
             
-            case 404:
-                throw new RestAuthResourceNotFound($resp);
-                
             // @codeCoverageIgnoreStart
-            default:
-                throw new RestAuthUnknownStatus($resp);
+        default:
+            throw new RestAuthUnknownStatus($resp);
         }
         // @codeCoverageIgnoreEnd
     }
@@ -423,6 +467,8 @@ class RestAuthGroup extends RestAuthResource
      *
      * @param mixed $group The group to remove. Either a 
      *    {@link RestAuthGroup} or a string representing the groupname.
+     *
+     * @return null
      *
      * @throws {@link RestAuthUnauthorized} When service authentication failed.
      * @throws {@link RestAuthResourceNotFound} When the group or subgroup does
@@ -442,15 +488,15 @@ class RestAuthGroup extends RestAuthResource
         $url = $this->name . '/groups/' . $groupname . '/';
         $resp = $this->_delete($url);
         switch ($resp->getResponseCode()) {
-            case 204:
-                return;
+        case 204:
+            return;
+        
+        case 404:
+            throw new RestAuthResourceNotFound($resp);
             
-            case 404:
-                throw new RestAuthResourceNotFound($resp);
-                
             // @codeCoverageIgnoreStart
-            default:
-                throw new RestAuthUnknownStatus($resp);
+        default:
+            throw new RestAuthUnknownStatus($resp);
         }
         // @codeCoverageIgnoreEnd
     }
