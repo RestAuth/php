e6c77e1f592a989167dd5e82535165485a2d087b
Mathias Ertl <mati@fsinf.at>
2011-08-01 19:44:15 +0200

    fix coding standard mistakes

--- RestAuth/restauth.php
+++ RestAuth/restauth.php
@@ -2,14 +2,35 @@
 /**
  * This file servers as a convenience file which imports the entire library.
  *
- * @package php-restauth
+ * PHP version 5.1
+ *
+ * LICENSE: php-restauth is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * php-restauth is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with php-restauth.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @category  Authentication
+ * @package   RestAuth
+ * @author    Mathias Ertl <mati@restauth.net>
+ * @copyright 2010-2011 Mathias Ertl
+ * @license   http://www.gnu.org/licenses/lgpl.html  GNU LESSER GENERAL PUBLIC LICENSE
+ * @version   0.0
+ * @link      https://php.restauth.net
  */
 
 /**
  * imports everything.
  */
-require_once('restauth_common.php');
-require_once('restauth_errors.php');
-require_once('restauth_users.php');
-require_once('restauth_groups.php');
+require_once 'restauth_common.php';
+require_once 'restauth_errors.php';
+require_once 'restauth_users.php';
+require_once 'restauth_groups.php';
 ?>
